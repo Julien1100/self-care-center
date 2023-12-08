27e304f1015fe75e360911e451381793a10a4af4
J8T <jul.milcent@gmail.com>
2023-12-08 10:30:40 +0100

    :lipstick: Add basic layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
 import { useState } from "react";
 import meditate from "./assets/meditate.svg";
-import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <h1>✨ Self Care Center ✨</h1>
+      <h3>Which type of message?</h3>
+
+      <div className="message-selector">
+        <form className="type-selector">
+          <input type="radio" name="message" id="affirmation" selected />
+          <label htmlFor="affirmation">affirmation</label>
+
+          <input type="radio" name="message" id="mantra" />
+          <label htmlFor="mantra">mantra</label>
+        </form>
+        <button>Recieve Message</button>
+
+        <div className="message-output">
+          <img src={meditate} />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
