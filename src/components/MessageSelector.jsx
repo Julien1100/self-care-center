d5fbee59370e263b5c78eed0068845bdf1af3c18
J8T <jul.milcent@gmail.com>
2023-12-08 12:40:41 +0100

    Add style to MessageSelector component

--- src/components/MessageSelector.jsx
+++ src/components/MessageSelector.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "../css/MessageSelector.css";
 
 const MessageSelector = ({
   currentType,
@@ -9,30 +10,32 @@ const MessageSelector = ({
   return (
     <div className="message-selector">
       <h3>Which type of message?</h3>
-      <form className="type-selector">
-        <input
-          type="radio"
-          name="message"
-          id="affirmation"
-          value="Affirmation"
-          checked={currentType === "Affirmation"}
-          onChange={() => handleTypeChange("Affirmation")}
-        />
-        <label htmlFor="affirmation">affirmation</label>
+      <div className="type-wrapper">
+        <div className="type-selector">
+          <input
+            type="radio"
+            name="message"
+            id="affirmation"
+            value="Affirmation"
+            checked={currentType === "Affirmation"}
+            onChange={() => handleTypeChange("Affirmation")}
+          />
+          <label htmlFor="affirmation">affirmation</label>
 
-        <input
-          type="radio"
-          name="message"
-          value="Mantra"
-          id="mantra"
-          checked={currentType === "Mantra"}
-          onChange={() => handleTypeChange("Mantra")}
-        />
-        <label htmlFor="mantra">mantra</label>
-      </form>
-      <button onClick={() => setCurrentData(getRandomData(currentType))}>
-        Receive Message
-      </button>
+          <input
+            type="radio"
+            name="message"
+            value="Mantra"
+            id="mantra"
+            checked={currentType === "Mantra"}
+            onChange={() => handleTypeChange("Mantra")}
+          />
+          <label htmlFor="mantra">mantra</label>
+        </div>
+        <button onClick={() => setCurrentData(getRandomData(currentType))}>
+          Receive Message
+        </button>
+      </div>
     </div>
   );
 };
